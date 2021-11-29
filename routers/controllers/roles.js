77ffc533d76769d1e3b6fb267e1899c90ecbef94
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-29 18:46:54 +0300

    create role controller

--- routers/controllers/roles.js
+++ routers/controllers/roles.js
@@ -1,7 +1,21 @@
 const rolesModel = require("./../../db/models/roles");
 
 const createRole = (req, res) => {
-  // code
+  const { role, permissions } = req.body;
+
+  const newRole = new rolesModel({
+    role,
+    permissions,
+  });
+
+  newRole
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 };
 
 module.exports = { createRole };
