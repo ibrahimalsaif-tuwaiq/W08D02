01d6d8587789c99a9076d4dd3c76657b1ee75904
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-29 19:03:08 +0300

    signup controller

--- routers/controllers/users.js
+++ routers/controllers/users.js
@@ -1,7 +1,33 @@
 const usersModel = require("./../../db/models/users");
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
 
-const signup = (req, res) => {
-  // code
+// Config .env file
+dotenv.config();
+
+// Get SALT variable from .env
+const SALT = Number(process.env.SALT);
+
+const signup = async (req, res) => {
+  const { email, password, role } = req.body;
+
+  const lowerCaseEmail = email.toLowerCase();
+  const hashedPassword = await bcrypt.hash(password, SALT);
+
+  const newUser = new usersModel({
+    email: lowerCaseEmail,
+    password: hashedPassword,
+    role,
+  });
+
+  newUser
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 };
 
 const login = (req, res) => {
--- routers/routes/users.js
+++ routers/routes/users.js
@@ -4,7 +4,7 @@ const { signup, login } = require("../controllers/users");
 
 const usersRouter = express.Router();
 
-usersRouter.post("/sginup", signup);
+usersRouter.post("/signup", signup);
 usersRouter.post("/login", login);
 
 module.exports = usersRouter;
