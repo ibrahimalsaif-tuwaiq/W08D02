388cf4f28e84e460902ad72d3be291c6c2a883ed
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-29 18:52:59 +0300

    users router

---
+++ routers/controllers/users.js
@@ -0,0 +1,11 @@
+const usersModel = require("./../../db/models/users");
+
+const signup = (req, res) => {
+  // code
+};
+
+const login = (req, res) => {
+  // code
+};
+
+module.exports = { signup, login };
